c061e806169c6c2d2da5575b9509cf7f3374a8ac
robin8778 <robin.007660@gmail.com>
2024-09-11 20:35:56 +0530

    first commit

---
+++ src/data/constants.js
@@ -0,0 +1,300 @@
+
+export const Bio = {
+  name: "Robin Singh",
+  roles: [
+    "Frontend Developer",
+  ],
+  description:
+    "I am a motivated and versatile individual, always eager to take on new challenges. With a passion for learning I am dedicated to delivering high-quality results. With a positive attitude and a growth mindset, I am ready to make a meaningful contribution and achieve great things.",
+  github: "https://github.com/constgenius",
+  resume:
+    "https://drive.google.com/file/d/1yRann9kBIbOgT8YzCdFPQDSB8zvVl3an/view?usp=sharing",
+  linkedin: "https://www.linkedin.com/in/robin-singh08/",
+  youtube: "https://www.youtube.com/@constGenius"
+};
+
+export const skills = [
+  {
+    skills: [
+      {
+        name: "HTML",
+        image: "./html.png",
+      },
+      {
+        name: "CSS",
+        image:
+          "./css.png",
+      },
+      {
+        name: "JavaScript",
+        image:
+          "./javascript.png",
+      },
+      {
+        name: "React Js",
+        image:
+          "./React.png",
+      },
+      {
+        name: "Redux",
+        image:
+          "./redux.png",
+      },
+      {
+        name: "Next Js",
+        image:
+          "./nextjs.png",
+      },
+      {
+        name: "Material UI",
+        image:
+          "./materialui.png",
+      },
+
+      {
+        name: "Node Js",
+        image: "./node.png",
+      },
+      {
+        name: "MySQL",
+        image:
+          "Mysql.png",
+      },
+      {
+        name: "MongoDB",
+        image:
+          "mongodb.png",
+      },
+      {
+        name: "Git",
+        image:
+          "./git.png",
+      },
+      {
+        name: "GitHub",
+        image:
+          "./github.png",
+      },
+      {
+        name: "VS Code",
+        image:
+          "./vscode.webp",
+      },
+      {
+        name: "Postman",
+        image:
+          "./postman.png",
+      },
+      {
+        name: "Figma",
+        image:
+          "./figma.png",
+      },
+      {
+        name: "Photoshop",
+        image:
+          "./photoshop.png",
+      },
+      {
+        name: "Illustrator",
+        image:
+          "./illustrator.png",
+      },
+      {
+        name: "Premiere Pro",
+        image:
+          "./premierepro.png",
+      },
+    ],
+  },
+];
+
+export const experiences = [
+  {
+    id: 0,
+    img: "./rise11.jpeg",
+    role: "Frontend Developer Intern",
+    company: "Rise11 Technologies",
+    date: "Jan 2023 - July 2023",
+    desc: "Developed the frontend of the Akashvaani website, a well-being astrology platform, using Next.js,Collaborated with the rise11 technology team to implement website features and functionality, Utilized Next.js, a popular React framework, to create responsive and interactive userinterfaces, Employed version control systems like Git to manage codebase and facilitate collaborative development.",
+    skills: [
+      "Next JS",
+      "Material UI",
+      "AWS",
+      "EC2",
+      "JavaScript",
+      "Node Js",
+    ],
+    doc: "https://media.licdn.com/dms/image/D4D2DAQFlp60ZqHuaFQ/profile-treasury-image-shrink_1280_1280/0/1691180828512?e=1692381600&v=beta&t=mM5Y_NE5EPlQhez5FAN6NLVSKcO_Ojt_9Gq3mnFGkAQ",
+  },
+
+];
+
+export const education = [
+  {
+    id: 0,
+    img: "./thaparlogo.png",
+    school: "Thapar Institute of Engineering & Technology",
+    date: "Aug 2021 - Jul 2023",
+    grade: "9.06 CGPA",
+    degree: "Master of Computer Application",
+  },
+  {
+    id: 1,
+    img: "./HPU.svg",
+    school: "Himachal Pradesh University",
+    date: "Jul 2017 - Aug 2020",
+    grade: "7.57 CGPA",
+    degree: "Bachelor of Computer Applications",
+
+  },
+  {
+    id: 2,
+    img: "./jnv.webp",
+    school: "Jawahar Navodaya Vidyalaya",
+    date: "Apr 2015 - Mar 2016",
+    grade: "78%",
+    degree: "Higher Secondary School Certificate (CBSE)",
+  },
+  {
+    id: 3,
+    img: "./jnv.webp",
+    school: "Jawahar Navodaya Vidyalaya",
+    date: "Apr 2013 - Mar 2014",
+    grade: "8.0 CGPA",
+    degree: "Secondary School Certificate (CBSE)",
+  },
+];
+
+export const projects = [
+  {
+    id: 7,
+    title: "constGenius",
+    description:
+      "Welcome to our web development channel! We offer high-quality tutorials and guides on various aspects of web development. From front-end technologies like HTML, CSS, and JavaScript (NextJS, React) to back-end frameworks and tools like Node.js, Express, and databases",
+    image:
+      "./constgenius.jpg",
+    tags: ["Web Development"],
+    category: "web app",
+    github: "https://www.youtube.com/@constGenius",
+    webapp: "https://www.youtube.com/@constGenius",
+  },
+  {
+    id: 10,
+    title: "Figma Clone",
+    description:
+      "A minimalistic Figma clone to show how to add real-world features like live collaboration with cursor chat, comments, reactions, and drawing designs (shapes, image upload) on the canvas using fabric.js.",
+    image:
+      "./FigmaClone.png",
+    tags: [
+      "NEXT.JS",
+      "TypeScript",
+      "Tailwind CSS",
+    ],
+    category: "web app",
+    github: "https://github.com/constgenius/FigmaClone",
+    webapp: "https://figma-clone-blond.vercel.app/",
+  },
+  {
+    id: 0,
+    title: "Youtube Clone",
+    description:
+      "The YouTube Home Page Clone with React, Tailwind CSS, and TypeScript is a focused project that recreates the iconic landing page of the YouTube platform. Leveraging the power of React for dynamic UI components, Tailwind CSS for rapid and responsive styling, and TypeScript for type-safe code, this clone provides developers with an opportunity to dissect and recreate the familiar YouTube home page experience.",
+    image:
+      "./YoutubeClone.png",
+    tags: [
+      "React",
+      "TypeScript",
+      "Tailwind CSS"
+    ],
+    category: "web app",
+    github: "https://github.com/constgenius/youtube_react_tailwind_ts",
+    webapp: "https://youtubereacttailwindts.vercel.app/",
+  },
+
+  {
+    id: 11,
+    title: "Travel Website",
+    description:
+      "The Fully Responsive Modern UI/UX Travel Website, developed with Next.js 14, Tailwind CSS, and TypeScript, exemplifies cutting-edge web development practices. This project combines the power of Next.js for server-side rendering with the utility-first approach of Tailwind CSS and the type safety of TypeScript to create a visually stunning and highly functional travel website.",
+    image:
+      "./TravelWebsite.png",
+    tags: [
+      "NEXT.JS",
+      "Tailwind CSS",
+      "TypeScript"
+    ],
+    category: "web app",
+    github: "https://github.com/constgenius/TravelWebsite",
+    webapp: "https://travel-website-ashy-nine.vercel.app/",
+  },
+
+  {
+    id: 1,
+    title: "Image Gallery",
+    description:
+      "Welcome to the Responsive Image Gallery in React.js project! In this project, we will create a fully responsive image gallery using React.js, allowing users to view and interact with a collection of images.",
+    image:
+      "./ImageGallery.jpg",
+    tags: [
+      "React Js",
+      "JavaScript",
+    ],
+    category: "web app",
+    github: "https://github.com/constgenius/ImageGallery_Using_React",
+    webapp: "https://image-gallery-using-react.vercel.app/",
+  },
+
+  {
+    id: 3,
+    title: "Shopping Cart",
+    description:
+      "An advanced shopping cart implemented with React and TypeScript is a powerful and sophisticated e- commerce solution.Leveraging React's dynamic UI capabilities and TypeScript's static typing, this shopping cart delivers a seamless and interactive online shopping experience.Users can easily browse, add, and manage products in the cart, while the checkout process is streamlined for efficiency.The combination of React and TypeScript ensures a modern, responsive, and error - resistant application, providing a feature - rich platform for both customers and online retailers.",
+    image:
+      "./ShoppingCart.jpg",
+    tags: ["React", "TypeScript"],
+    category: "web app",
+    github: "https://github.com/constgenius/ShoppingCartReact",
+    webapp: "https://shoppingcartreact.vercel.app/",
+  },
+
+  {
+    id: 4,
+    title: "MCAVerse",
+    description:
+      "'Mcaverse,' a cutting-edge website project, showcases my expertise in leveraging Next.js and Tailwind CSS to create dynamic and visually stunning web applications. With a strong foundation in modern web development, I have skillfully integrated these technologies to deliver an immersive and user-friendly online experience.",
+    image:
+      "./MCA.jpg",
+    tags: ["React Js", "Tailwind CSS"],
+    category: "web app",
+    github: "https://mcaverse.vercel.app/",
+    webapp: "https://mcaverse.vercel.app/",
+  },
+  {
+    id: 5,
+    title: "Movie Search App",
+    date: "Jun 2021",
+    description:
+      "This Movie Search App is a beginner-friendly React JS project that enables users to explore movies by title. It utilizes the power of the OMDb API for movie data retrieval, offering features like searching for movies, displaying detailed information about selected films, and presenting a list of popular movies. With its responsive design, it adapts seamlessly to various screen sizes. This project serves as an excellent learning opportunity for newcomers to React and API integration, allowing them to build a functional movie search application from scratch.",
+    image:
+      "./MovieSearchApp.jpg",
+    tags: ["React Js"],
+    category: "web app",
+    github: "https://github.com/constgenius/MovieSearchApp/",
+    webapp: "https://github.com/constgenius/MovieSearchApp/",
+  },
+  {
+    id: 6,
+    title: "Tic Tac Toe",
+    description:
+      "Tic Tac Toe Game: Create an interactive and classic Tic Tac Toe game using HTML, CSS, and JavaScript. Players take turns to place X or O symbols on a 3x3 grid, aiming to form a winning row, column, or diagonal sequence. The game includes user-friendly interface elements and real-time feedback, making it an engaging and enjoyable experience.",
+    image:
+      "./TicTacToe.jpg",
+    tags: ["HTML", "CSS", "JavaScript"],
+    category: "web app",
+    github: "https://github.com/constgenius/TicTacToe_JavaScript",
+    webapp: "https://constgenius.github.io/TicTacToe_JavaScript/",
+  },
+
+];
+
